8eb3117abf3e175b11566968bb50d5b9db8d070f
kema-dev <github@kema.dev>
2025-02-14 00:49:04 +0100

    up

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
+import "log"
 
 func main() {
 	log.Println("Hello, World!")
-
-	var unusedVar string
-	file, _ := os.Open("nonexistentfile.txt")
-	fmt.Println(file)
 }
