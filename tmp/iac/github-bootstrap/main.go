cedab0549b371f31f06708c3bb33c109d528fd3c
kema-dev <github@kema.dev>
2025-02-23 21:53:33 +0100

    up

--- tmp/iac/github-bootstrap/main.go
+++ tmp/iac/github-bootstrap/main.go
@@ -55,7 +55,7 @@ func bootstrapGithubRepository(ctx *pulumi.Context, provider *github.Provider, a
 
 		Description: pulumi.String(args.Description),
 		HomepageUrl: pulumi.String(args.HomepageUrl),
-		Topics: func() pulumi.StringArray {
+		Topics: func() pulumi.StringArrayInput {
 			var topics pulumi.StringArray
 			for _, topic := range args.Topics {
 				topics = append(topics, pulumi.String(topic))
@@ -72,7 +72,6 @@ func bootstrapGithubRepository(ctx *pulumi.Context, provider *github.Provider, a
 		SquashMergeCommitTitle:   pulumi.String("PR_TITLE"),
 		SquashMergeCommitMessage: pulumi.String("COMMIT_MESSAGES"),
 		AllowUpdateBranch:        pulumi.Bool(true),
-		DefaultBranch:            pulumi.String("main"),
 		DeleteBranchOnMerge:      pulumi.Bool(true),
 		HasDiscussions:           pulumi.Bool(true),
 		HasIssues:                pulumi.Bool(true),
@@ -80,40 +79,27 @@ func bootstrapGithubRepository(ctx *pulumi.Context, provider *github.Provider, a
 		HasWiki:                  pulumi.Bool(false),
 		WebCommitSignoffRequired: pulumi.Bool(false),
 
-		VulnerabilityAlerts: func() pulumi.BoolPtrInput {
+		VulnerabilityAlerts: func() pulumi.Bool {
 			if args.Visibility == "public" {
-				return pulumi.BoolPtr(true)
+				return pulumi.Bool(true)
 			}
 			// Advanced Security is required for private repositories
-			return pulumi.BoolPtr(false)
+			return pulumi.Bool(false)
 		}(),
-		SecurityAndAnalysis: github.RepositorySecurityAndAnalysisArgs{
-			AdvancedSecurity: func() github.RepositorySecurityAndAnalysisAdvancedSecurityPtrInput {
-				if args.Visibility == "public" {
-					return nil
-				}
-				// Advanced Security is required for private repositories
-				return nil
-			}(),
-			SecretScanning: func() github.RepositorySecurityAndAnalysisSecretScanningPtrInput {
-				if args.Visibility == "public" {
-					return github.RepositorySecurityAndAnalysisSecretScanningArgs{
+		SecurityAndAnalysis: func() *github.RepositorySecurityAndAnalysisArgs {
+			if args.Visibility == "public" {
+				return &github.RepositorySecurityAndAnalysisArgs{
+					SecretScanning: github.RepositorySecurityAndAnalysisSecretScanningArgs{
 						Status: pulumi.String("enabled"),
-					}
-				}
-				// Advanced Security is required for private repositories
-				return nil
-			}(),
-			SecretScanningPushProtection: func() github.RepositorySecurityAndAnalysisSecretScanningPushProtectionPtrInput {
-				if args.Visibility == "public" {
-					return github.RepositorySecurityAndAnalysisSecretScanningPushProtectionArgs{
+					},
+					SecretScanningPushProtection: github.RepositorySecurityAndAnalysisSecretScanningPushProtectionArgs{
 						Status: pulumi.String("enabled"),
-					}
+					},
 				}
-				// Advanced Security is required for private repositories
-				return nil
-			}(),
-		},
+			}
+			// Advanced Security is required for private repositories
+			return nil
+		}(),
 	}, pulumi.Provider(provider))
 	if err != nil {
 		return nil, err
