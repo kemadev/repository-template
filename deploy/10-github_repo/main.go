65041350611180329d3aa1a9ae8f1041a5725add
kema-dev <github@kema.dev>
2025-02-26 17:54:10 +0100

    up

--- deploy/10-github_repo/main.go
+++ deploy/10-github_repo/main.go
@@ -19,23 +19,6 @@ func main() {
 					"go",
 				},
 			},
-			Actions: repo.ActionsArgs{
-				Actions: []string{
-					"anchore/sbom-action@*",
-					"anchore/scan-action@*",
-					"aws-actions/configure-aws-credentials@*",
-					"DavidAnson/markdownlint-cli2-action@*",
-					"docker://rhysd/actionlint@*",
-					"golangci/golangci-lint-action@*",
-					"googleapis/release-please-action@*",
-					"goreleaser/goreleaser-action@*",
-					"hadolint/hadolint-action@*",
-					"ibiqlik/action-yamllint@*",
-					"pulumi/actions@*",
-					"semgrep/semgrep@*",
-					"trufflesecurity/trufflehog@*",
-				},
-			},
 		})
 		if err != nil {
 			return err
