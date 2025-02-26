12e47de83b5f5933adeafb30135432931a636c89
kema <github@kema.dev>
2025-02-26 22:56:48 +0100

    up (#38)

---
+++ deploy/10-github_org/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/kemadev/iac-components/pkg/github/org"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		err := org.Wrapper(ctx, org.WrapperArgs{
+			Settings: org.SettingsArgs{
+				Company:      "kemadev",
+				Description:  "Making cloud infrastructure a breeze!",
+				Email:        "contact@kema.dev",
+				BillingEmail: "billing@kema.dev",
+				Blog:         "https://www.kema.dev",
+				Location:     "France",
+			},
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return
+}
